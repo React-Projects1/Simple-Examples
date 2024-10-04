93a624dcd316e6650eea5e5874e696d38a4dead0
Marwa Refaieh <marwarefaieh@gmail.com>
2024-10-04 18:21:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // App 1
+  const [counter, setCounter] = useState(0);
+  const [style, setStyle] = useState(false);
+
+  const plus = () => {
+    setCounter(counter + 1);
+    setStyle(!style);
+  };
+
+  // App 2
+  const [data, setData] = useState({
+    fname: "",
+    lname: "",
+  });
+
+  // App 3
+  const [number, setNumber] = useState([]);
+  const random = () => {
+    setNumber([...number, { value: Math.floor(Math.random()* 10) }]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-1">
+        <h2>App 1</h2>
+        <div className="content">
+          <button onClick={plus}>+</button>
+          <p
+            style={
+              style
+                ? { backgroundColor: "#dda2dd" }
+                : { backgroundColor: "linen" }
+            }
+          >
+            {counter}
+          </p>
+        </div>
+      </div>
+
+      <div className="app-2">
+        <h2>App 2</h2>
+        <div className="content">
+          <input
+            type="text"
+            placeholder="Enter your first name"
+            onChange={(e) => {
+              setData({ ...data, fname: e.target.value });
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter your last name"
+            onChange={(e) => {
+              setData({ ...data, lname: e.target.value });
+            }}
+          />
+          <p>
+            {data.fname} {data.lname}
+          </p>
+        </div>
+      </div>
+      <div className="app-3">
+        <h2>App 3</h2>
+        <div className="content">
+          <button onClick={random}>Add Number</button>
+          <ul>
+            {number.map((item) => 
+              <li>{item.value}</li>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
